7068b8bf3dcc7f75f26f734fe61d0067dd41af4a
Leonardo Lamoia Rosa <leonardotcinf@gmail.com>
2024-05-28 18:54:25 -0300

    Toast notifications (sonner)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./global.css";
 import { RouterProvider } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { router } from "./routes";
+import { Toaster } from 'sonner'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors/>
       <RouterProvider router={router} />
     </HelmetProvider>
   );
